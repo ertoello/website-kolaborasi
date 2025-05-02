faa68c564ee5858a3760e73fb66cf446e30466d1
ertoello <ertoello17fx@gmail.com>
2025-05-03 00:51:10 +0700

    test deploy 16

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,17 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "development") {
-  app.use(
-    cors({
-      origin: [
-        // "http://localhost:5173",
-        "https://website-kolaborasi.vercel.app",
-        "https://w3lc3pgc-5173.asse.devtunnels.ms",
-      ],
-      credentials: true,
-    })
-  );
-}
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://website-kolaborasi.vercel.app",
+      "https://w3lc3pgc-5173.asse.devtunnels.ms",
+    ],
+    credentials: true,
+  })
+);
+
 
 app.use(express.json({ limit: "5mb" })); // ✅ Sekarang express.json() tidak error
 app.use(cookieParser());
