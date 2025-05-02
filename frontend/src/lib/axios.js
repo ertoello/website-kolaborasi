33de5d88b5167648bf31ef253bcf3c9c19aea2ff
ertoello <ertoello17fx@gmail.com>
2025-05-02 22:47:19 +0700

    test deploy 9

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,10 +3,9 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      // ? "https://w3lc3pgc-5000.asse.devtunnels.ms/api/v1" // kalau lagi testing online (DevTunnel)
-      // ? "http://localhost:5000/api/v1"                // atau ini kalau lokal
-      ? "website-kolaborasi.co.id/api/v1"                // atau ini kalau lokal
-      : "/api/v1",
+      ? // ? "https://w3lc3pgc-5000.asse.devtunnels.ms/api/v1" // kalau lagi testing online (DevTunnel)
+        "http://localhost:5000/api/v1" // atau ini kalau lokal
+      : "https://website-kolaborasi.co.id/",
   withCredentials: true,
 });
 
